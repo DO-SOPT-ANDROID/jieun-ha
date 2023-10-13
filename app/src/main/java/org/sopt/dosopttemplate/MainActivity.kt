8409faa242fcd5565a1ab44830cc631e960e5f8e
HAJIEUN02 <jieundaeun@naver.com>
2023-10-13 22:52:29 +0900

    week1 essential assignment

--- app/src/main/java/org/sopt/dosopttemplate/MainActivity.kt
+++ app/src/main/java/org/sopt/dosopttemplate/MainActivity.kt
@@ -1,11 +1,23 @@
 package org.sopt.dosopttemplate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.sopt.dosopttemplate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val name = intent.getStringExtra("name")
+        val mbti = intent.getStringExtra("mbti")
+        val id = intent.getStringExtra("idresult")
+
+        binding.name.text = name
+        binding.userIdContent.hint = id
+        binding.userMbtiContent.hint = mbti
     }
 }
