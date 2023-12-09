f37cb9920d1ef63768c9517d669715357c7787ab
JIEUNI <jieundaeun@naver.com>
2023-12-09 17:19:55 +0900

    {#5}[feat]로그인 UI layer 분리

--- app/src/main/java/org/sopt/dosopttemplate/AuthService.kt
+++ app/src/main/java/org/sopt/dosopttemplate/AuthService.kt
@@ -1,7 +1,6 @@
 package org.sopt.dosopttemplate
 
 import org.sopt.dosopttemplate.data.RequestLoginDto
-import org.sopt.dosopttemplate.data.RequestMemberCheckDto
 import org.sopt.dosopttemplate.data.RequestMemberDto
 import org.sopt.dosopttemplate.data.RequestSignUpDto
 import org.sopt.dosopttemplate.data.ResponseLoginDto
@@ -11,6 +10,7 @@ import org.sopt.dosopttemplate.data.ResponseSignUpDto
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Query
 
@@ -18,7 +18,7 @@ interface AuthService {
     @POST("api/v1/members")
     fun signup(
         @Body request: RequestSignUpDto,
-    ):Call<ResponseSignUpDto> // 응답이 왓을 때 Callback으로 불려질 타입
+    ):Call<ResponseSignUpDto> // 응답이 왓을 때 Callback으로 불려질 타입*/
 
     @POST("api/v1/members/sign-in")
     fun login(
--- app/src/main/java/org/sopt/dosopttemplate/AuthViewModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/AuthViewModel.kt
@@ -1,11 +1,16 @@
 package org.sopt.dosopttemplate
 
+import android.content.Intent
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.material.internal.ContextUtils
 import org.sopt.dosopttemplate.ServicePool.authService
+import org.sopt.dosopttemplate.Utils.showToast
 import org.sopt.dosopttemplate.data.RequestLoginDto
+import org.sopt.dosopttemplate.data.RequestSignUpDto
 import org.sopt.dosopttemplate.data.ResponseLoginDto
+import org.sopt.dosopttemplate.data.ResponseSignUpDto
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -41,5 +46,42 @@ class AuthViewModel : ViewModel() {
     val isLoginButtonClicked: MutableLiveData<Boolean> = MutableLiveData(false)
     fun onLoginButtonClick() {
         isLoginButtonClicked.value = true
+    }
+
+    private val _signUpResult: MutableLiveData<ResponseSignUpDto> = MutableLiveData()
+    val signUpResult: LiveData<ResponseSignUpDto> get() = _signUpResult
+
+    private val _signUpSuccess: MutableLiveData<Boolean> = MutableLiveData()
+    val signUpSuccess: LiveData<Boolean> get() = _signUpSuccess
+
+    fun signUp(id: String, pw: String, name: String){
+        authService.signup(RequestSignUpDto(id, pw, name))
+            .enqueue(object: Callback<ResponseSignUpDto>{
+                override fun onResponse(
+                    call: Call<ResponseSignUpDto>,
+                    response: Response<ResponseSignUpDto>
+                ) {
+                    if (response.isSuccessful) {
+                        when(response.code()){
+                            201 -> {
+                                // 회원가입 성공
+                                _signUpResult.value = response.body()
+                                _signUpSuccess.value = true
+                            }
+
+                            400 -> {
+                                // 회원가입 실패
+                                _signUpSuccess.value = false
+                            }
+
+                            else -> {
+                                _signUpSuccess.value = false
+                            }
+                        }
+                    }
+                }
+                override fun onFailure(call: Call<ResponseSignUpDto>, t: Throwable) {
+                }
+            })
     }
 }
